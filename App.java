917c71f7c0e23b4e94e915c1f0b73205fdde3f0c
k-rsouza <k.souza@edu.pucrs.br>
2023-05-02 19:56:54 -0300

    segundo commit

---
+++ App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextInt()) {
+            System.out.println("Digite o valor de V: ");
+            int v = in.nextInt();
+            System.out.println("Digite o valor de T: ");
+            int t = in.nextInt();
+
+            
+        }
+    }
+}
