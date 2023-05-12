e7ea6092ea166ac320271ef1b905d20c9fa0ed71
h11ribeiro <h9ribeiro@gmail.com>
2023-05-12 13:39:27 -0300

    12 commit

---
+++ Teste.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class Teste{   
+  Main a = new Main();
+  
+
+  @test
+  public void hasNextInt(){
+      assertEquals(1, a.quandoFazer(10, 3, 60));
+      
+         }
+    }
+
